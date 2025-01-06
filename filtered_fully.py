7582b20af9b287c7add1e00083f31d1934cb005d
ShathaSaad <85974132+ShathaSaad@users.noreply.github.com>
2025-01-06 22:24:40 +0000

    filtered_fully.py

--- filtered_fully.py
+++ filtered_fully.py
@@ -5,19 +5,26 @@ from collections import defaultdict
 from constraint_evaluation import constraint_evaluation
 from operators import operators
 import bisect
+import os
+
 
 
 class filtered_fully:
     def __init__(self):
         self.applyOperator = operators()
 
-    def check_predicates(self, sorted_possible_refinements, statistical_tree, expression, datasize, dataName):
-        counter = 0
+    def check_predicates(self, sorted_possible_refinements, statistical_tree, expression, datasize, dataName, result_num, query_num, constraint, predicates, combination, distribution, Correlation, constraint_type):
+        counter, i = 0, 0
         agg_counter = 0
         satisfied_conditions = []
-        found = False
+        check_counter_Not = 0
         check_counter = 0
         refinement_counter = 0
+        solutions_count = 0
+        end_time = 0
+        checked_num = 0
+        accesses_num = 0
+        refinment_num = 0
 
         # Create dictionaries for parent-child relationships and cluster info
         parent_child_map = defaultdict(list)
@@ -35,73 +42,185 @@ class filtered_fully:
         start_time = time.time()
         
         for refinement in sorted_possible_refinements:
-            # Loop over each column in the refinement dictionary
-            
-            condition1_operator = refinement['refinements'][0]['operator']
-            condition1 = refinement['refinements'][0]['value'] 
-            condition2_operator = refinement['refinements'][1]['operator']
-            condition2 = refinement['refinements'][1]['value'] 
-            #print(condition1_operator, condition1, condition2_operator, condition2)
+            # Loop over each condition in the refinement dictionary   
+            conditions = []
+            operators = []
+            for ref in refinement['refinements']:
+                conditions.append(ref['value'])
+                operators.append(ref['operator'])
+
+            #print(conditions)
+
             similarity = refinement['distance']
 
             filtered_clusters = []          
             for root_key in root_clusters:
-
-                filtered_clusters, counter = self.filter_clusters_Hash(root_key, cluster_map, condition1, condition1_operator, condition2, 
-                condition2_operator, filtered_clusters, counter, parent_child_map)
+                filtered_clusters, counter = self.filter_clusters_Hash(root_key, cluster_map, conditions, operators, filtered_clusters, counter, parent_child_map)
                 
             # Convert the filtered clusters to a DataFrame and save to CSV
             filtered_df = pd.DataFrame(filtered_clusters)
 
             # Evaluate and store the results
             evaluate_constraint = constraint_evaluation() 
-            #satisfied, agg_counter = evaluate_constraint.cardinality(filtered_df, counter, agg_counter, condition1, condition2)
             check_counter += 1
-            satisfied, agg_counter = evaluate_constraint.evaluate_constraint(filtered_df, expression, condition1, condition2, agg_counter, similarity, "full")
+            #satisfied, agg_counter = evaluate_constraint.cardinality(filtered_df, counter, agg_counter, condition1, condition2)
+            satisfied, agg_counter, Not_satisfied, result = evaluate_constraint.evaluate_constraint1(filtered_df, expression, conditions, agg_counter, similarity, "full", constraint_type)
 
             if satisfied != []:
+                #check_counter_satisfy += 1
                 refinement_counter += 1
                 satisfied_conditions.append(satisfied)
-                #if len(satisfied_conditions) == 7:
-                    #break
-                
-            #filename = f'filtered_fully_income_{condition1}_children_{condition2}.csv'
-            #filename = filename.replace(" ", "_").replace(",", "_")
-                # Save to CSV
-            #filtered_df.to_csv(filename, index=False)
+                solutions_count +=1
+
+
+                if len(satisfied_conditions) == result_num:
+                    end_time = time.time() 
+                    checked_num = check_counter
+                    accesses_num = counter
+                    refinment_num = refinement_counter
+                    break
         
-        end_time = time.time() 
+        if end_time == 0: #no solutions
+            end_time = time.time()
+            checked_num = check_counter
+            accesses_num = counter
+            refinment_num = refinement_counter
+                    
+        self.print_results(satisfied_conditions, start_time, end_time, dataName, datasize, query_num, combination, 
+                accesses_num, solutions_count, checked_num, check_counter_Not, refinment_num, constraint, predicates, distribution, result_num, Correlation)
+            
 
 
+    def print_results(self, satisfied_conditions, start_time, end_time, dataName, datasize, query_num, combination, 
+        counter, solutions_count, check_counter, check_counter_Not, refinement_counter, constraint, predicates, distribution, result_num, Correlation):
+                    
+        '''
+        directory = '/Users/Shatha/Downloads/Query_Refinment_Shatha/running_files'
+        filename = f'filtered_fully_{conditions}.csv'
+        filename = filename.replace(" ", "_").replace(",", "_")
+        # Save to CSV
+        # Join the directory and file name to create the full path
+        full_path = os.path.join(directory, filename)
+        filtered_df.to_csv(full_path, index=False)
+        '''
         # Save satisfied conditions to CSV
         satisfied_conditions_df = pd.DataFrame(satisfied_conditions)
-        satisfied_conditions_df.to_csv("satisfied_conditions_Fully.csv", index=False)
+        output_directory = "/Users/Shatha/Downloads/Query_Refinment_Shatha/sh_Final1"
+        # For the second file
+        file_path_2 = os.path.join(
+            output_directory, 
+            f"satisfied_conditions_Fully_{dataName}_size{datasize}_query{query_num}_constraint{constraint}.csv"
+        )
+        satisfied_conditions_df.to_csv(file_path_2, index=False)
+
         elapsed_time = end_time - start_time
-        
+
         info = []
         refinement_info = {
             "Data Name": dataName,
             "Data Size": datasize,
-            "Access No.": counter,
-            "Checked No.": check_counter,
-            "Refinement No.": refinement_counter,
-            "Time": round(elapsed_time, 3)
+            "Query Num": query_num,
+            "Type": "Fully",
+            "Top-K": result_num,
+            "Combinations Num": combination,
+            "Distance": round((check_counter) / combination * 100, 3),
+            "Access Num": counter,
+            "Checked Num": (check_counter),
+            "Refinement Num": refinement_counter,
+            "Time": round(elapsed_time, 3),
+            "Constraint Width": round(constraint[1]-constraint[0], 2),
+            "Solutions Count": solutions_count,
+            "Constraint": constraint, 
+            #"Distribution": distribution,
+            #"Correlation": Correlation,
+            "Query": predicates,
+            "Range Evaluation Time": " ",
+            "Division Time": " ",
+            "Single Time": " ",
+            "Processing Time": " "
         }
         info.append(refinement_info)
-        # Save info to CSV
         info_df = pd.DataFrame(info)
-        info_df.to_csv("Fully_info.csv", mode='a', index=False, header=False)
+        output_directory = "/Users/Shatha/Downloads/Query_Refinment_Shatha/sh_Final1"
+        # Ensure the directory exists
+        os.makedirs(output_directory, exist_ok=True)
 
+        # Define the full file path including the directory
+        file_path = os.path.join(output_directory, f"Run_info_{dataName}_size{datasize}_H.csv")
+
+        write_header = not os.path.exists(file_path)  # Write header only if the file does not exist
+
+        # Write to CSV with appropriate header setting
+        info_df.to_csv(file_path, mode='a', index=False, header=write_header)
+
+        print("\nTop-", result_num," :")
         print("Number of boxes access: ", counter)
         print("Number of checked", check_counter)
+        #print("Checked No.-Not:", check_counter_Not)
+        #print("Checked No.-Satisfy:", check_counter_satisfy)
         print("Number of refinments", refinement_counter)
         print("Time taken Overall:", round(elapsed_time, 3), "seconds")  
-        #print("Number of Aggregation calculated: ", agg_counter)
 
 
-    def filter_clusters_Hash(self, current_key, cluster_map, condition1, condition1_operator, condition2, condition2_operator, filtered_clusters, 
+    def filter_clusters_Hash(self, current_key, cluster_map, conditions, operators, filtered_clusters, 
     counter, parent_child_map):
     
+        stack = [current_key]
+        conditions1 = enumerate(zip(conditions, operators))
+
+
+        while stack:
+            counter += 1
+            current_key = stack.pop()
+            current_cluster = cluster_map[current_key]
+
+            # Initialize a flag to track if the current cluster satisfies all conditions
+            fully_satisfies = True
+
+            # Loop through each condition and operator
+            for idx, (condition, operator) in conditions1:
+                data_min = current_cluster['Data_Min'][idx]  # Get the corresponding Data_Min value for this condition
+                data_max = current_cluster['Data_Max'][idx]  # Get the corresponding Data_Max value for this condition
+                
+
+                # If any condition is not satisfied, mark as False and break
+                if not self.applyOperator.apply_operator(data_min, data_max, condition, operator, "Full"):
+                    fully_satisfies = False
+                    break
+
+            # If all conditions are satisfied, mark the cluster as "Full"
+            if fully_satisfies:
+                current_cluster['Satisfy'] = 'Full'
+                filtered_clusters.append(current_cluster)
+
+            else:  # Otherwise, check the child clusters
+                stack.extend(parent_child_map.get(current_key, []))
+
+        return filtered_clusters, counter
+    
+    def filter_clusters_Hash1(self, current_key, cluster_map, condition1, condition1_operator, condition2, condition2_operator, condition3, condition3_operator, filtered_clusters, counter, parent_child_map):
+
+        stack = [current_key]
+
+        while stack:
+            counter += 1
+            current_key = stack.pop()
+            current_cluster = cluster_map[current_key]
+
+            if (self.applyOperator.apply_operator(current_cluster['Data_Min'][0], current_cluster['Data_Max'][0], condition1, condition1_operator, "Full") and 
+                self.applyOperator.apply_operator(current_cluster['Data_Min'][1], current_cluster['Data_Max'][1], condition2, condition2_operator, "Full") and 
+                self.applyOperator.apply_operator(current_cluster['Data_Min'][2], current_cluster['Data_Max'][2], condition3, condition3_operator, "Full")):
+                current_cluster['Satisfy'] = 'Full'
+                filtered_clusters.append(current_cluster)
+
+                
+            else: # Otherwise, check the child clusters
+                stack.extend(parent_child_map.get(current_key, []))
+
+        return filtered_clusters, counter
+
+    def filter_clusters_Hash2(self, current_key, cluster_map, condition1, condition1_operator, condition2, condition2_operator, filtered_clusters, counter, parent_child_map):
+
         stack = [current_key]
 
         while stack:
@@ -114,13 +233,12 @@ class filtered_fully:
                 current_cluster['Satisfy'] = 'Full'
                 filtered_clusters.append(current_cluster)
 
-               
+                
             else: # Otherwise, check the child clusters
                 stack.extend(parent_child_map.get(current_key, []))
 
         return filtered_clusters, counter
-    
-        
+            
     def check_predicates_inc(self, sorted_possible_refinements, statistical_tree, descendants, expression):
         counter = 0
         agg_counter = 0
