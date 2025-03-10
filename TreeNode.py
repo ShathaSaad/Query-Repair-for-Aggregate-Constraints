f77dc22a0e5172aa81c94308e9eb19275fec8c0a
ShathaSaad <85974132+ShathaSaad@users.noreply.github.com>
2025-03-10 01:59:50 +0000

    Delete TreeNode.py

--- TreeNode.py
+++
@@ -1,6 +0,0 @@
-class TreeNode:
-    def __init__(self, level, cluster_id, data_points):
-        self.level = level
-        self.cluster_id = cluster_id
-        self.data_points = data_points
-        self.children = []
